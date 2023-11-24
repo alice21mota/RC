61e27808d90778f1d10333448f44e769893c17ba
Ana Margarida Almeida <ana.margarida.almeida@tecnico.ulisboa.pt>
2023-11-24 10:49:01 +0000

    Add client

---
+++ project/client.cpp
@@ -0,0 +1,92 @@
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <time.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <signal.h>
+
+#include <iostream>
+using namespace std;
+
+string port = "58000";
+string ip = "localhost";
+
+string sendUDP(string message)
+{
+    message = message + "\n";
+
+    // maybe global variables
+    int fd, errcode;
+    ssize_t n;
+    socklen_t addrlen;
+    char buffer[128];
+
+    struct addrinfo hints, *res;
+    struct sockaddr_in addr;
+
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (fd == -1)
+    {
+        exit(1);
+    }
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // IPv4
+    hints.ai_socktype = SOCK_DGRAM;
+
+    errcode = getaddrinfo(ip.c_str(), port.c_str(), &hints, &res);
+    if (errcode != 0)
+    {
+        exit(1);
+    }
+
+    n = sendto(fd, message.c_str(), message.length(), 0, res->ai_addr, res->ai_addrlen);
+    if (n == -1)
+    {
+        exit(1);
+    }
+
+    addrlen = sizeof(addr);
+    n = recvfrom(fd, buffer, sizeof(buffer), 0, (struct sockaddr *)&addr, &addrlen);
+    if (n == -1)
+    {
+        exit(1);
+    }
+
+    cout << "UDP returned: " << buffer << endl; // Debug
+
+    freeaddrinfo(res);
+    close(fd);
+
+    return buffer;
+}
+
+void getArgs(int argc, char *argv[])
+{
+    for (int i = 1; i < argc; i += 2)
+    {
+        if (!strcmp(argv[i], "-n"))
+            ip = argv[i + 1];
+        if (!strcmp(argv[i], "-p"))
+            port = argv[i + 1];
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    string result;
+
+    getArgs(argc, argv);
+
+    // cout << port << ip << endl;
+
+    result = sendUDP("hello");
+    cout << "--------" << result << endl; // Debug
+}
