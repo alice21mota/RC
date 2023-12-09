82ac706be52e404b9e20a127f9550eff28bd5d5d
Ana Margarida Almeida <ana.margarida.almeida@tecnico.ulisboa.pt>
2023-12-09 21:21:59 +0000

    Trying to fix read Fdata

--- project/serverSide/server.cpp
+++ project/serverSide/server.cpp
@@ -72,7 +72,7 @@ string getTCPCommand(string command) {
     if (whichCommand == "OPA") {
         string user, password, name, start_value, timeactive, Fname, Fsize, Fdata;
         iss >> user >> password >> name >> start_value >> timeactive >> Fname >> Fsize;
-        Fdata = command.substr(command.size() - stoi(Fsize));
+        Fdata = command.substr(command.size() - stoi(Fsize), stoi(Fsize));
         response = open(user, password, name, start_value, timeactive, Fname, Fsize, Fdata);
     }
     else response = "ERR";
