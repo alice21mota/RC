d2a1e1123bdee71ea90203e83a24cd1a2556f15a
Alice Mota <alice.arnaud.mota@gmail.com>
2023-12-13 21:05:42 +0000

    Minor changes

--- project/clientSide/commands/commandsTCP.cpp
+++ project/clientSide/commands/commandsTCP.cpp
@@ -1,6 +1,6 @@
 #include "commandsTCP.h"
 
-string open(string command){
+string open(string command) {
     // name asset_fname start_value timeactive
     // name = max 10 alphanumeric
     // asset_fname = name of image or document file
@@ -11,88 +11,101 @@ string open(string command){
     string toReturn, fSize;
     istringstream iss(command);
 
-    if (logged_in == true){
+    if (logged_in == true) {
 
         if (iss >> whichCommand >> name >> asset_fname >> start_value >> timeactive && iss.eof()) {
-            
-            if (isDescription(name) && isFileName(asset_fname) && isStartValue(start_value) && isDuration(timeactive)){
-                
+
+            if (isDescription(name) && isFileName(asset_fname) && isStartValue(start_value) && isDuration(timeactive)) {
+
                 if (fileExists(asset_fname)) {
-                    
+
                     fName = asset_fname;
                     size_t fileSize = getFileSize(fName);
-                    if (isFileSizeValid(fileSize)){
-                        
+                    if (isFileSizeValid(fileSize)) {
+
                         fSize = fileSizeString(asset_fname);
 
-                        if (fSize != "Too many digits"){
+                        if (fSize != "Too many digits") {
 
                             toReturn = "OPA " + userID + " " + password + " " + name + " " + start_value + " " +
-                                    timeactive + " " + fName + " " + fSize + " ";
+                                timeactive + " " + fName + " " + fSize + " ";
 
                             return toReturn;
 
-                        } else return "File size is too big";
+                        }
+                        else return "File size is too big";
 
-                    } else return "Invalid File Size";
+                    }
+                    else return "Invalid File Size";
 
-                } else return "File not found";
+                }
+                else return "File not found";
 
-            } else return "Invalid fields";   
+            }
+            else return "Invalid fields";
 
-        } else return "Command not as expected";
+        }
+        else return "Command not as expected";
 
-    } else return "User not logged in";
+    }
+    else return "User not logged in";
 }
 
-string close(string command){
-    
-    string whichCommand, aid;    
+string close(string command) {
+
+    string whichCommand, aid;
     istringstream iss(command);
 
-    if (logged_in == true ){
+    if (logged_in == true) {
 
         if (iss >> whichCommand >> aid && iss.eof()) {
-            
-            if (isValidAID(aid)){
+
+            if (isValidAID(aid)) {
 
                 tempAID = aid; //Used to return error to user if needed
                 return "CLS " + userID + " " + password + " " + aid;
 
-            } else return "Incorrect AID";               
+            }
+            else return "Incorrect AID";
 
-        } else return "Incorrect command format";
+        }
+        else return "Incorrect command format";
 
-    } else return "User not logged in ";
+    }
+    else return "User not logged in ";
 }
 
-string bid(string command){
+string bid(string command) {
 
     string whichCommand, aid, value;
     istringstream iss(command);
 
-    if (logged_in == true ){
+    if (logged_in == true) {
 
         if (iss >> whichCommand >> aid >> value && iss.eof()) {
-            
-            if (isValidAID(aid)){
-                
+
+            if (isValidAID(aid)) {
+
                 tempAID = aid;
 
-                if (isBidValue(value)){
+                if (isBidValue(value)) {
 
                     return "BID " + userID + " " + password + " " + aid + " " + value;
-                
-                } else return "Invalid value";
 
-            } else return "Invalid AID";
+                }
+                else return "Invalid value";
 
-        } else return "Incorrect command format";
+            }
+            else return "Invalid AID";
 
-    } else return "User not logged in";
+        }
+        else return "Incorrect command format";
+
+    }
+    else return "User not logged in";
 }
 
-string show_asset(string command){
+string show_asset(string command) {
 
     string whichCommand, aid;
     istringstream iss(command);
@@ -101,12 +114,14 @@ string show_asset(string command){
     if (iss >> whichCommand >> aid && iss.eof()) {
 
         tempAID = aid;
-        
-        if (isValidAID(aid)){
+
+        if (isValidAID(aid)) {
 
             return "SAS " + aid;
 
-        } else return "Invalid AID";
+        }
+        else return "Invalid AID";
 
-    } else return "Incorrect command format";
+    }
+    else return "Incorrect command format";
 }
--- project/common/validations.cpp
+++ project/common/validations.cpp
@@ -9,16 +9,16 @@ bool isAlphanumeric(string str) {
     return (all_of(str.begin(), str.end(), ::isalnum));
 }
 
-bool isUID(string uid){
-    return (uid.size()==6 && isNumeric(uid));
+bool isUID(string uid) {
+    return (uid.size() == 6 && isNumeric(uid));
 }
 
-bool isPassword(string password){
-    return (password.size()== 8 && isAlphanumeric(password));
+bool isPassword(string password) {
+    return (password.size() == 8 && isAlphanumeric(password));
 }
 
-bool isDescription(string description){
-    if (description.size() > 0 && description.size() <= 10){
+bool isDescription(string description) {
+    if (description.size() > 0 && description.size() <= 10) {
         if (isAlphanumeric(description))
             return true;
         return false;
@@ -26,13 +26,13 @@ bool isDescription(string description){
     return false;
 }
 
-bool isFileName(string fileName){
+bool isFileName(string fileName) {
     const regex pattern("^([0-9]{1,20}\\.)?[a-zA-Z0-9_-]{1,20}\\.[a-zA-Z]{3}$");
     return regex_match(fileName, pattern);
 }
 
-bool isStartValue(string startValue){
-    if (startValue.size() > 0 && startValue.size() <= 6){
+bool isStartValue(string startValue) {
+    if (startValue.size() > 0 && startValue.size() <= 6) {
         if (isNumeric(startValue))
             return true;
         return false;
@@ -40,8 +40,8 @@ bool isStartValue(string startValue){
     return false;
 }
 
-bool isDuration(string duration){
-    if (duration.size() > 0 && duration.size() <= 5){
+bool isDuration(string duration) {
+    if (duration.size() > 0 && duration.size() <= 5) {
         if (isNumeric(duration))
             return true;
         return false;
@@ -49,7 +49,7 @@ bool isDuration(string duration){
     return false;
 }
 
-bool isValidState(string state){
+bool isValidState(string state) {
     if (state == "0" || state == "1")
         return true;
     return false;
@@ -59,7 +59,7 @@ bool fileExists(string filePath) {
     return filesystem::exists(filePath);
 }
 
-bool isFileSizeValid(size_t fileSize){
+bool isFileSizeValid(size_t fileSize) {
     size_t maxSize = 10 * 1024 * 1024; //10MB in bytes
     return fileSize <= maxSize;
 }
@@ -73,43 +73,45 @@ bool isValidFileSize(string fileSizeStr) {
         int maxSize = 10 * 1024 * 1024; // 10MB in bytes
         return fileSize >= 0 && fileSize <= maxSize;
 
-    } catch (const invalid_argument& e) {
-        // Handle the case where the conversion fails
+    }
+    catch (const invalid_argument& e) {
+     // Handle the case where the conversion fails
         cerr << "Error converting file " << endl;
         return false;
 
-    } catch (const out_of_range& e) {
-        // Handle the case where the converted value is out of the range of int
+    }
+    catch (const out_of_range& e) {
+     // Handle the case where the converted value is out of the range of int
         cerr << "Value out of range for int" << endl;
         return false;
     }
 }
 
-bool isValidAID(string aid){
+bool isValidAID(string aid) {
     return (isNumeric(aid) && aid.size() == 3);
 }
 
 bool isValidDate(string date) {
-   
+
     // Define the regular expression for the date format "YYYY-MM-DD"
     regex dateRegex(R"(\d{4}-\d{2}-\d{2})");
-    
+
     return regex_match(date, dateRegex);
 
 }
 
-bool isValidTime(string time){
-    
+bool isValidTime(string time) {
+
     regex timeRegex(R"(\d{2}:\d{2}:\d{2})");
 
     // Use std::regex_match to check if the input string matches the pattern
     return regex_match(time, timeRegex);
 }
 
-bool isValidSecTime(string sec_time){
+bool isValidSecTime(string sec_time) {
     return (isNumeric(sec_time) && sec_time.size() <= 5);
 }
 
-bool isBidValue(string value){
+bool isBidValue(string value) {
     return (isNumeric(value) && value.size() <= 6);
 }
